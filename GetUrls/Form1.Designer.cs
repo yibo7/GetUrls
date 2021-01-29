94fea13426ed2f86acd2e91b86ea1a8a0c35856e
cqs263 <cqs263@163.com>
2021-01-29 12:31:40 +0800

    初始化项目

---
+++ GetUrls/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace GetUrls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSoure = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRz = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtSoure
+            // 
+            this.txtSoure.Location = new System.Drawing.Point(12, 37);
+            this.txtSoure.Multiline = true;
+            this.txtSoure.Name = "txtSoure";
+            this.txtSoure.Size = new System.Drawing.Size(809, 179);
+            this.txtSoure.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请输入HTML代码";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "获取结果";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(746, 222);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 31);
+            this.btnGet.TabIndex = 5;
+            this.btnGet.Text = "获取";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(94, 227);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(334, 21);
+            this.txtHost.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "补全相对路径";
+            // 
+            // txtRz
+            // 
+            this.txtRz.Location = new System.Drawing.Point(15, 313);
+            this.txtRz.Name = "txtRz";
+            this.txtRz.Size = new System.Drawing.Size(806, 162);
+            this.txtRz.TabIndex = 8;
+            this.txtRz.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 498);
+            this.Controls.Add(this.txtRz);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSoure);
+            this.Name = "Form1";
+            this.Text = "链接获取工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSoure;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtRz;
+    }
+}
+
---
+++ GetUrls/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetUrls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string sHost = txtHost.Text.Trim();
+            if (string.IsNullOrEmpty(sHost))
+            {
+                MessageBox.Show("补全相对路径不能为空,请输入如:http://www.baidu.com");
+                return;
+            }
+            List<string> lst = GetUrls(txtSoure.Text.Trim(), sHost);
+
+            txtRz.Text = string.Join("\r\n", lst.ToArray());
+
+        }
+
+        private string GetUrlHost(string url)
+
+        {
+
+            string text = url;
+            string pattern = @"(?<=http://)[\w\.]+[^/]"; //C#正则表达式提取匹配URL的模式，       
+
+            string s = "";
+
+            MatchCollection mc = Regex.Matches(text, pattern);//满足pattern的匹配集合        
+
+            foreach (Match match in mc)
+
+            {
+
+                s = match.ToString();
+
+            }
+
+            return s;
+
+            //textBox1.Text = s;     
+
+        }
+
+        public List<string> GetUrls(string sContent,string tHost)
+        {
+            Regex reg = new Regex(@"(?is)<a[^>]*?href=(['""]?)(?<url>[^'""\s>]+)\1[^>]*>(?<text>(?:(?!</?a\b).)*)</a>");
+            MatchCollection mc = reg.Matches(sContent);
+            Uri baseUri = new Uri(tHost);
+            List<string> urls = new List<string>();
+
+            foreach (Match m in mc)
+            {
+
+                Uri uriPage = new Uri(baseUri, m.Groups["url"].Value);
+                urls.Add(uriPage.ToString());
+            }
+
+            return (from i in urls select i).Distinct().ToList();
+        }
+    }
+}
